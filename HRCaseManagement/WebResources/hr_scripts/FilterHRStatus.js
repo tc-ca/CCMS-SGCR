5c1fc402fd24be6cd95d4c7970e49866ffd23b6e
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2020-06-22 13:58:32 +0000

    Update Solution -

---
+++ HRCaseManagement/WebResources/hr_scripts/FilterHRStatus.js
@@ -0,0 +1,8 @@
+function FilterHRStatus(eContext, fieldName, value){
+    var formContext = eContext.getFormContext();   
+
+    var field = formContext.getAttribute(fieldName);
+    if (field == null || field == 'undefined') return;
+
+    formContext .getControl(fieldName).removeOption(value);
+}
---
+++ HRCaseManagement/WebResources/hr_scripts/ToggleDisplayBPF.js
@@ -0,0 +1,8 @@
+//Toggles display of the BPF. Takes the formcontext and the the name of the twoOption field that was changed as a string.
+function showHideBusinessProcessFlow(eContext, twoOptionFieldName) {
+    var formContext = eContext.getFormContext(); 
+    var twoOptionFieldValue = formContext.getAttribute(twoOptionFieldName).getValue();
+
+    if (twoOptionFieldValue == null || twoOptionFieldValue == 'undefined') return;
+    formContext.ui.process.setVisible(twoOptionFieldValue); //Shows BPF if twoOptionFieldValue is true, hides if false
+}
