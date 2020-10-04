a71f7c3fd387ebf0deee99301c0020fb8e5c5efc
tc-buildbot <nepasrepondre-noreply@tc.gc.ca>
2020-10-04 17:00:30 +0000

    Update Solution -  Create new business rules

--- HRCaseManagement/WebResources/hr_scripts/FilterHRStatus.js
+++ HRCaseManagement/WebResources/hr_scripts/FilterHRStatus.js
@@ -1,9 +1,48 @@
-'use strict';
-function FilterHRStatus(eContext, fieldName, value){
-    var formContext = eContext.getFormContext();   
-
-    var field = formContext.getAttribute(fieldName);
-    if (field == null || field == 'undefined') return;
-
-    formContext .getControl(fieldName).removeOption(value);
-}
+"use strict";
+var CCMS;
+(function (CCMS) {
+    var HRCase;
+    (function (HRCase) {
+        var Form;
+        function FilterHRStatus(eContext) {
+            Form = eContext.getFormContext();
+            var control = Form.getControl("header_hr_casestatus");
+            control.removeOption(315840002 /* Saved */);
+        }
+        HRCase.FilterHRStatus = FilterHRStatus;
+        function ShowCloseCaseConfirmation(eContext) {
+            Form = eContext.getFormContext();
+            var statusfield = Form.getAttribute("hr_casestatus");
+            var resolutionField = Form.getAttribute("hr_resolution");
+            var closedonField = Form.getAttribute("hr_closedon");
+            //When case status is set to closed
+            if (statusfield.getValue() != 315840004 /* Closed */) {
+                return;
+            }
+            if (closedonField.getValue() != null) {
+                return;
+            }
+            var globalContext = Xrm.Utility.getGlobalContext();
+            var confirmStrings = {
+                text: "Are you sure you want to close this case?",
+                title: "Close case confirmation",
+            };
+            if (globalContext.userSettings.languageId == 1036) {
+                confirmStrings.text = "Are you sure you want to close this case? (fr)";
+                confirmStrings.title = "Close case confirmation (fr)";
+            }
+            var confirmOptions = { height: 200, width: 450 };
+            Xrm.Navigation.openConfirmDialog(confirmStrings, confirmOptions).then(function (success) {
+                if (success.confirmed) {
+                    console.log("Dialog closed using OK button.");
+                    closedonField.setValue(new Date());
+                    Form.data.save();
+                }
+                else {
+                    //do nothing
+                }
+            });
+        }
+        HRCase.ShowCloseCaseConfirmation = ShowCloseCaseConfirmation;
+    })(HRCase = CCMS.HRCase || (CCMS.HRCase = {}));
+})(CCMS || (CCMS = {}));
